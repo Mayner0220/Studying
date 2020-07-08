e79181cc43c5c733dd354c2663f5b9d6864a6698
Mayner <shj030220@gmail.com>
2020-07-08 16:55:57 +0900

    TF2 Movie Review Text Classification

--- Tensorflow2_study/Movie_Review_Text_Classification.py
+++ Tensorflow2_study/Movie_Review_Text_Classification.py
@@ -69,18 +69,6 @@ val_loss = history_dict['val_loss']
 
 epochs = range(1, len(acc) + 1)
 
-plt.plot(epochs, loss, 'bo', label='Training loss')
-plt.plot(epochs, val_loss, 'b', label='Validation loss')
-plt.title('Training and validation loss')
-plt.xlabel('Epochs')
-plt.ylabel('Loss')
-plt.legend()
-
-plt.show()
-
-plt.clf()
-
-plt.plot(epochs, acc, 'bo', label='Training acc')
 plt.plot(epochs, val_acc, 'b', label='Validation acc')
 plt.title('Training and validation accuracy')
 plt.xlabel('Epochs')
